7abb45ee4f8ccd6df75338f7753338bf61409ddd
mounir beggas <mbeggas@hotmail.com>
2020-11-11 12:50:50 +0100

    Modification on validation exception management method

--- src/main/java/com/softlines/fastpos/controller/AdditiveController.java
+++ src/main/java/com/softlines/fastpos/controller/AdditiveController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,7 +29,7 @@ public class AdditiveController {
     ExceptionManagement exceptionManagement = new ExceptionManagement();
 
     @PostMapping("/save")
-    public ResponseEntity<Additive> addAdditive(@RequestBody Additive additive) {
+    public ResponseEntity<Additive> addAdditive(@Valid @RequestBody Additive additive) {
 
         try {
 
--- src/main/java/com/softlines/fastpos/exceptionmanagement/FastposExceptionHandler.java
+++ src/main/java/com/softlines/fastpos/exceptionmanagement/FastposExceptionHandler.java
@@ -11,25 +11,27 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class FastposExceptionHandler {
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public final ResponseEntity<Object> handleHeaderException()
+    public final ResponseEntity<List<String>> handleHeaderException()
     {
         return new ResponseEntity("Media Type Not Supported: Must be Json", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public final ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+        List<String> errors = new ArrayList<>();
         for (ObjectError error : ex.getBindingResult().getAllErrors()) {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
+            errors.add(errorMessage);
         }
         return new ResponseEntity(errors, HttpStatus.UNPROCESSABLE_ENTITY);
     }
