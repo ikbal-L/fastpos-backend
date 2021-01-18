ed18c7d1030e9e04be18320e9748e5a5b676b2e4
Iqbal Lachraf <prog2.71828@gmail.com>
2021-01-18 11:37:58 +0100

    undo previous commit

--- src/main/java/com/softlines/fastpos/domain/Product.java
+++ src/main/java/com/softlines/fastpos/domain/Product.java
@@ -26,11 +26,11 @@ import java.util.List;
 
 @SQLDelete(sql = "UPDATE Product SET deleted=true WHERE id=?")
 @Where(clause = "deleted = false")
-@Table(uniqueConstraints = {
-        @UniqueConstraint(columnNames = {
-                "category_id", "rank"
-        })
-})
+//@Table(uniqueConstraints = {
+//        @UniqueConstraint(columnNames = {
+//                "category_id", "rank"
+//        })
+//})
 @Entity
 public class Product extends BaseEntity {
 
