7018edbdcbfc9048f87cddfa465fb67b67c7defa
user2-71828 <70612927+user2-71828@users.noreply.github.com>
2021-02-04 16:34:32 +0100

    Fixing custom repository delete

--- src/main/java/com/softlines/fastpos/repository/em/CustomRepositoryImp.java
+++ src/main/java/com/softlines/fastpos/repository/em/CustomRepositoryImp.java
@@ -2,20 +2,20 @@ package com.softlines.fastpos.repository.em;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.persistence.metamodel.Attribute;
 import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.Metamodel;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 
 public class CustomRepositoryImp<T, ID> implements CustomRepository<T, ID> {
 
     private  JpaRepository<T, ID> repository;
+
 
     private EntityManagerFactory entityManagerFactory;
 
@@ -33,30 +33,19 @@ public class CustomRepositoryImp<T, ID> implements CustomRepository<T, ID> {
         var associations = associations(em, (Class<T>) entity.getClass());
         for (String association : associations) {
             String query = buildQuery(association,foreignKeyName,id);
-            var q = em.createNativeQuery(query).setParameter("id", id);
+            var q = em.createNativeQuery(query);
             q.executeUpdate();
         }
         em.getTransaction().commit();
     }
 
     private String buildQuery(String tableName, String foreignKeyName, ID id) {
-        StringBuilder sb = new StringBuilder();
-        sb
-                .append("UPDATE FROM ")
-                .append("`")
-                .append(tableName)
-                .append("`")
-                .append(" SET ")
-                .append("`")
-                .append(foreignKeyName)
-                .append("`")
-                .append(" = NULL")
-                .append(" WHERE ")
-                .append("`")
-                .append(foreignKeyName)
-                .append("`")
-                .append(" = :id");
-        return sb.toString();
+      String query = "update `:table` set :foreignKey = NULL  where `:foreignKey` = :id";
+      query =query
+              .replace(":table",tableName)
+              .replace(":foreignKey",foreignKeyName)
+              .replace(":id",id.toString());
+        return query;
     }
 
     private String getTableName(EntityManager em, Class<T> entityClass) {
@@ -70,11 +59,13 @@ public class CustomRepositoryImp<T, ID> implements CustomRepository<T, ID> {
     private String[] associations(EntityManager em, Class<T> entityClass) {
         Metamodel meta = em.getMetamodel();
         EntityType<T> entityType = meta.entity(entityClass);
+        var attributes = entityType.getAttributes().stream().filter(Attribute::isAssociation).collect(Collectors.toList());
+
         List<String> associations = new ArrayList<>();
-        ManyToOne[] manyToOneAssociations = entityClass.getAnnotationsByType(ManyToOne.class);
-        for (ManyToOne manyToOne : manyToOneAssociations) {
-            String targetEntityTableName = getTableName(em, manyToOne.targetEntity());
-            associations.add(targetEntityTableName);
+        for (Attribute<? super T, ?> attribute : attributes) {
+
+            String associationTableName = getTableName(em, ((ListAttribute) attribute).getBindableJavaType());
+            associations.add(associationTableName);
         }
         return associations.toArray(String[]::new);
     }
