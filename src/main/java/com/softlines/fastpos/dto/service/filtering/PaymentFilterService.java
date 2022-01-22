0b4c6bfc1f6bc81dcbf873a52fd7cbceff1fcd44
user2-71828 <70612927+user2-71828@users.noreply.github.com>
2022-01-22 17:48:17 +0100

    fix small issues

--- src/main/java/com/softlines/fastpos/dto/service/filtering/PaymentFilterService.java
+++ src/main/java/com/softlines/fastpos/dto/service/filtering/PaymentFilterService.java
@@ -40,23 +40,17 @@ public class PaymentFilterService extends FilterService<Payment, PaymentFilter> 
             predicates.add(datePredicate);
         }
 
-        if (deliverymanId.isPresent()&& deliverymanIds.isEmpty()){
-            Predicate deliverymanIdPredicate = this.criteriaBuilder.equal(root.<Deliveryman>get("deliveryman").<Long>get("id"), deliverymanId.get());
-            predicates.add(deliverymanIdPredicate);
-        }
 
-        if (deliverymanIds.isPresent() && deliverymanId.isEmpty()){
+
+        if (deliverymanIds.isPresent()&& !deliverymanIds.get().isEmpty()){
             var deliverymanIdsPredicate = this.criteriaBuilder.in(root.<Deliveryman>get("deliveryman").<Long>get("id"));
             deliverymanIds.get().forEach(deliverymanIdsPredicate::value);
             predicates.add(deliverymanIdsPredicate);
         }
 
-        if (customerId.isPresent()&& customerIds.isEmpty()){
-            Predicate customerIdPredicate = this.criteriaBuilder.equal(root.<Deliveryman>get("customer").<Long>get("id"), customerId.get());
-            predicates.add(customerIdPredicate);
-        }
 
-        if (customerIds.isPresent() && customerId.isEmpty()){
+
+        if (customerIds.isPresent()&& !customerIds.get().isEmpty()){
             var customerIdsPredicate = this.criteriaBuilder.in(root.<Deliveryman>get("customer").<Long>get("id"));
             customerIds.get().forEach(customerIdsPredicate::value);
             predicates.add(customerIdsPredicate);
